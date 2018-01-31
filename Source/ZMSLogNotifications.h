dd48f8384c70648edd0cd2f46378333ac5947312
Jacob Persson <jacob.persson@wire.com>
2018-01-31 17:33:43 +0100

    Add a description key

--- Source/ZMSLogNotifications.h
+++ Source/ZMSLogNotifications.h
@@ -18,5 +18,6 @@
 
 #import <Foundation/Foundation.h>
 
+extern NSString * const ZMLoggingDescriptionKey;
 extern NSString * const ZMLoggingRequestLoopNotificationName;
 extern NSString * const ZMLoggingInconsistentStateNotificationName;
--- Source/ZMSLogNotifications.m
+++ Source/ZMSLogNotifications.m
@@ -18,5 +18,6 @@
 
 #import "ZMSLogNotifications.h"
 
+NSNotificationName const ZMLoggingDescriptionKey = @"ZMLoggingDescription";
 NSNotificationName const ZMLoggingRequestLoopNotificationName = @"ZMLoggingRequestLoopNotificationName";
 NSNotificationName const ZMLoggingInconsistentStateNotificationName = @"ZMLoggingInconsistentStateNotificationName";
